d44656264b9214f2ed760736b672ab292694add4
danielfu13 <daniel.fu123@hotmail.com>
2022-05-24 23:33:04 -0400

    third commit

---
+++ Assets/js/script.js
@@ -0,0 +1,16 @@
+var highscoreEl = document.querySelector("#highscore");
+var timerEl = document.querySelector("#timer");
+var mainEL = document.querySelector("#main");
+var timeLeft = 90;
+var totalPoints = 0;
+
+var start = function() {
+    var container = document.createElement("div");
+    
+}
+
+
+
+var home = function() {
+    
+}
