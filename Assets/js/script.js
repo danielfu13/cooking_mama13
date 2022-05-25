7174c3da45d6471a5c84d1fb70b0e9cfecaeabaf
danielfu13 <daniel.fu123@hotmail.com>
2022-05-25 18:23:19 -0400

    2nd commit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,51 @@
 var highscoreEl = document.querySelector("#highscore");
 var timerEl = document.querySelector("#timer");
 var mainEL = document.querySelector("#main");
-var timeLeft = 90;
+var timeRemain = 0;
 var totalPoints = 0;
 
-var start = function() {
-    var container = document.createElement("div");
-    
+var questions = [{
+    question1: "What can you store in an array?",
+    option1: ["1. booleans", "2. strings", "3. numbers", "4. All of the above"],
+    answer1: "4. All of the above"    
+},
+{
+    question2: "Which of the following is a common application coders use to upload and download files?",
+    option2: ["1. VScode", "2. Google", "3. Github", "4. jQuery"],
+    answer2:  "3. Github"
+},
+{
+    question3: "Which of the following is mainly known for changing the appearance of a webpage?",
+    option3: ["1. CSS", "2. HTML", "3. Javascript", "4. Java"],
+    answer3: "1. CSS"
+},
+{
+    question4: "Which syntax is used to create a link in an HTML file?",
+    option4: ["1. <section>", "2. <html>", "3. <media>", "4. <a href="],
+    answer4: "4. <a href="
+},
+{
+    question5: "What is the git command to switch between different branches?",
+    option5: ["1. gin init", "2. git checkout", "3 git commit", "4. ls"],
+    answer5: "2. git checkout"
 }
+];
 
+var startQuiz = function() {
+    timeRemain = 60;
+    document.getElementById('timer').innerHTML = timeRemain;
+
+    timeRemain = setInterval(function() {
+        document.getElementById("timer").innerHTML = timeRemain;
+
+        if (timeRemain <= 0) {
+            clearInterval(timeRemain);
+            endQuiz();
+        }
+    }, 1000);
+    
+    return;
+}
 
 
 var home = function() {
