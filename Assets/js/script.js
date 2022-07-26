6f0872ce13dea400d1646acb84879351753307bb
danielfu13 <daniel.fu123@hotmail.com>
2022-07-26 13:48:20 -0400

    fixed html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,76 +1,50 @@
-var highscoreEl = document.querySelector("#highscore");
-var timerEl = document.querySelector("#timer");
-var mainEL = document.querySelector("#main");
-var timeRemain = 0;
-var totalPoints = 0;
-var quizQuestions = -1;
+var startBtn = document.getElementById("startBtn");
+var countdownTimer = document.getElementById("countdownTimer");
+var homeContainer =  document.getElementById("homeContainer");
+var quizContainer = document.getElementById("quizContainer");
+var questionTitle = document.getElementById("questionTitle");   
+var high_scores= [];
+var output="";
+var score = 0;
+let i = 0;
 
+// set options
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var option4 = document.getElementById("option4");
+var answers = document.getElementById("answers"); 
+
+// set timer functions
+var time = 75;
+var time_remaining = true;
+var time_start= false;
+
+// questions array
 var questions = [{
-    question1: "What can you store in an array?",
-    option1: ["1. booleans", "2. strings", "3. numbers", "4. All of the above"],
-    answer1: "4. All of the above"    
+    question: "What are people who write computer code called?",
+    option: ["1. Programmers", "2. Professors", "3. Writers", "4. Creators"],
+    answers: 0
 },
 {
-    question2: "Which of the following is a common application coders use to upload and download files?",
-    option2: ["1. VScode", "2. Google", "3. Github", "4. jQuery"],
-    answer2:  "3. Github"
+    question: "Which of the following is a common application coders use to upload and download files?",
+    option: ["1. VScode", "2. Google", "3. Github", "4. jQuery"],
+    answers:  2
 },
 {
-    question3: "Which of the following is mainly known for changing the appearance of a webpage?",
-    option3: ["1. CSS", "2. HTML", "3. Javascript", "4. Java"],
-    answer3: "1. CSS"
+    question: "Which of the following is mainly known for changing the appearance of a webpage?",
+    option: ["1. CSS", "2. HTML", "3. Javascript", "4. Java"],
+    answers: 0
 },
 {
-    question4: "Which syntax is used to create a link in an HTML file?",
-    option4: ["1. <section>", "2. <html>", "3. <media>", "4. <a href="],
-    answer4: "4. <a href="
+    question: "Which syntax is used to create a link in an HTML file?",
+    option: ["1. <section>", "2. <html>", "3. <a href=", "4. <media>"],
+    answers: 2
 },
 {
-    question5: "What is the git command to switch between different branches?",
-    option5: ["1. gin init", "2. git checkout", "3 git commit", "4. ls"],
-    answer5: "2. git checkout"
+    question: "What is the git command to switch between different branches?",
+    option: ["1. gin init", "2. git checkout", "3 git commit", "4. ls"],
+    answers: 1
 }
 ];
-
-function game() {
-    quizQuestions++;
-
-    if (quizQuestions > questions.length - 1) {
-        endQuiz();
-        return;
-    }
-
-    }
-
-
-var startQuiz = function() {
-    startBtn.addEventListener("click", event => {
-        timer();
-        mainEL.remove();
-        questions(question1);
-        startTime();
-    });
-}
-
-
-function startTime () {
-    timeRemain = 75;
-    document.getElementById('timeRemain').innerHTML = timeRemain;
-
-    timer = setInterval(function() {
-        timeRemain--;
-        document.getElementById("timeRemain").innerHTML = timeRemain;
-
-        if (timeRemain <= 0) {
-            clearInterval(timeRemain);
-            endQuiz();
-        }
-    }, 1000);
-    
-    game();
-}
-
-startQuiz();
-
-
 
